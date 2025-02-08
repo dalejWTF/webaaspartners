3d360937ae7552f8ef3285d6dbf7139a4d7e19b8
Daniel Ulloa <danielulloam@gmail.com>
2025-02-07 22:54:16 -0500

    correccion para uso de idioma

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -6,22 +6,29 @@ import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 import LanguageSwitcher from "./LanguageSwitcher";
+import { useTranslations } from "next-intl"; // Importa el hook para las traducciones
 
-const links = [
-  { name: "home", path: "/" },
-  { name: "services", path: "/services" },
-  { name: "contact us", path: "/contact" }
-];
-
-const MobileNav = ({ locale }) => {
+const MobileNav = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const t = useTranslations('Header'); // Traducciones de la sección de navegación
+
   const [open, setOpen] = React.useState(false); // Para manejar el estado del menú
-  
+
+  // Extraemos el idioma de la URL
+  const locale = pathname.split('/')[1] || 'en'; // 'en' es el idioma por defecto
+
+  // Lista de enlaces con idioma dinámico
+  const links = [
+    { name: "Home", path: `/${locale}` },
+    { name: "Services", path: `/${locale}/services` },
+    { name: "Contact", path: `/${locale}/contact` }
+  ];
+
   // Función para manejar el cambio de idioma
   const handleLanguageChange = (languageCode) => {
     const currentPath = window.location.pathname;
-    // Redirigir al idioma seleccionado
+    // Redirigir al idioma seleccionado manteniendo la ruta actual
     router.push(`/${languageCode}${currentPath.replace(/^\/[a-z]{2}/, '')}`);
     setOpen(false); // Cerrar el menú
   };
@@ -37,7 +44,7 @@ const MobileNav = ({ locale }) => {
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
+          <Link href={`/${locale}`}>
             <h1 className="text-4xl font-semibold text-accent">
               A+AS <span>Partners</span>
             </h1>
@@ -55,13 +62,13 @@ const MobileNav = ({ locale }) => {
                 ${link.path === pathname && "text-white border-b-2 border-accent"}
                 text-xl capitalize hover:text-white transition-all`}
             >
-              {link.name}
+              {t(link.name)}
             </Link>
           ))}
         </nav>
 
         {/* Dropdown de idioma */}
-        <LanguageSwitcher/>
+        <LanguageSwitcher onLanguageChange={handleLanguageChange} />
       </SheetContent>
     </Sheet>
   );
