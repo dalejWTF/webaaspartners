c6fc6bdd3ed835421eb5737872164fb78dad857b
Daniel Ulloa <danielulloam@gmail.com>
2025-02-07 22:19:43 -0500

    modificación para menú de traducción en computadoras y moviles

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,13 +1,14 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { Select, SelectTrigger, SelectContent, SelectItem, SelectGroup } from "@/components/ui/select"; // Asegúrate de que este `Select` esté disponible en tu proyecto
 import { GrLanguage } from "react-icons/gr";
 
 
 const LanguageSwitcher = () => {
     const router = useRouter();
+    const pathname = usePathname(); // Obtener el pathname actual para determinar el idioma actual
 
     // Idiomas disponibles
     const languages = [
@@ -22,10 +23,14 @@ const LanguageSwitcher = () => {
         router.push(`/${languageCode}${currentPath.replace(/^\/[a-z]{2}/, '')}`);
     };
 
+    // Obtener el idioma actual desde el pathname
+    const currentLanguage = pathname.split('/')[1] || 'en'; // Si no hay idioma en el path, usar 'en' por defecto
+
     return (
-        <Select onValueChange={handleChangeLanguage}>
-            <SelectTrigger>
+        <Select value={currentLanguage} onValueChange={handleChangeLanguage}>
+            <SelectTrigger className="flex items-center gap-2 xl:text-primary text-accent">
                 <GrLanguage />
+                <span>{currentLanguage.toUpperCase()}</span> {/* Mostrar el idioma seleccionado */}
             </SelectTrigger>
             <SelectContent className="w-auto p-0">
                 <SelectGroup>
