f1b587847e463778b6992ac8b9f76b37ca2a62e0
Daniel Ulloa <danielulloam@gmail.com>
2025-02-07 21:53:34 -0500

    Se agrega libreria y modal para traducciones en pantallas grandes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
-import Link from 'next/link'
-import { Button } from './ui/button'
-import Nav from './Nav'
-import MobileNav from './MobileNav'
+"use client";
+import React from 'react';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import Nav from './Nav';
+import MobileNav from './MobileNav';
+import { usePathname } from 'next/navigation';
+import LanguageSwitcher from './LanguageSwitcher';
+import { useTranslations } from "next-intl"; // Importa el hook
 
+const Header = ({ locale = 'en' }) => {
+  const t = useTranslations('Header'); // Obtén las traducciones para esta página
+  const pathname = usePathname();
 
-const Header = () =>{
-    return <header className="py-8 xl:py-12">
-        <div className="container mx-auto flex justify-between items-center">
-            {/* logo */}
-            <Link href="/">
-                <h1 className="text-4xl font-semibold">
-                    A+AS <span className="text-accent">Partners</span>
-                </h1>
-            </Link>
+  return (
+    <header className="py-8 xl:py-12">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href={`/${locale}`}>
+          <h1 className="text-4xl font-semibold">
+            A+AS <span className="text-accent">Partners</span>
+          </h1>
+        </Link>
 
-            {/* desktop nav & contact*/}
-            <div className="hidden xl:flex items-center gap-8 text-accent">
-                <Nav/>
-                <Link href="/contact">
-                    <Button variant='outline' className="text-primary"> Contact Us</Button>
-                </Link>
-            </div>
-
-            {/* desktop nav & contact*/}
-            <div className="xl:hidden">
-                <MobileNav/>
-            </div>
+        {/* desktop nav & contact */}
+        <div className="hidden xl:flex items-center gap-8 text-accent">
+          <Nav locale={locale} />
+          <Link href={`/${locale}/contact`}>
+            <Button variant="outline" className="text-primary">
+              {t("Contact")}
+            </Button>
+          </Link>
+          {/* Dropdown de idioma */}
+          <LanguageSwitcher />
         </div>
+
+        {/* mobile nav & contact */}
+        <div className="xl:hidden">
+          <MobileNav locale={locale} />
+        </div>
+      </div>
     </header>
-    
-}
+  );
+};
 
-
-export default Header
+export default Header;
