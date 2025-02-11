c8db32ecfd770063743ec540812ce8cb2c83c2b7
daulloa <daniel.ulloa@coopmego.fin.ec>
2025-02-11 12:30:43 -0500

    feat: cambio de presentación para la imagen en una sola columna

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -4,17 +4,17 @@ import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 
-const PageTransition = ({children}) => {
+const PageTransition = ({ children }) => {
     const pathname = usePathname();
     return (
         <AnimatePresence>
             <div key={pathname}>
-                <motion.div 
-                    initial={{opacity: 1}} 
+                <motion.div
+                    initial={{ opacity: 1 }}
                     animate={{
-                        opacity:0,
-                        transition: {delay: 1, duration: 0.4, ease: "easeInOut"}
-                }} className="h-screen w-screen fixed bg-background top-0 pointer-events-none"
+                        opacity: 0,
+                        transition: { delay: 1, duration: 0.4, ease: "easeInOut" }
+                    }} className="h-screen w-screen fixed bg-background top-0 pointer-events-none"
                 />
                 {children}
             </div>
