64009fecdf448522b7831256f2db59156eb044d2
Daniel Ulloa <danielulloam@gmail.com>
2026-02-19 22:25:02 -0500

    update button reference on page for service

--- src/app/(frontend)/[locale]/services/page.jsx
+++ src/app/(frontend)/[locale]/services/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, useCallback } from 'react';
-import { Button } from '../../../components/ui/button';
+import { Button } from '@/components/ui/button';
 import {
   Carousel,
   CarouselContent,
